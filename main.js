7ec1947c92393cc4fa1d54f26a25aa2a7432a91e
Pablo Tabares H <pablother3@gmail.com>
2021-11-09 21:35:27 -0500

    background canvas

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ scene.add(pointLight, ambientLight)
 
 const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50)
-scene.add(lightHelper, gridHelper)
+scene.add(lightHelper)
 
 
 const controls = new OrbitControls(camera, renderer.domElement)
